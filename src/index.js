66c0d80c9f9e24c7447c1177f6ec0e21053fff36
yuntingc <chiayunting@gmail.com>
2022-12-06 20:38:15 +0800

    added oauth

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENTID}
+      redirectUri={window.location.origin}
+      audience={process.env.REACT_APP_AUDIENCE}
+      scope={process.env.REACT_APP_SCOPE}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
